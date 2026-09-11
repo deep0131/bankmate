02e06ee912614e83cd3599263902e38323b71c10
Anuj Kuralkar <76731106+anuj-kuralkar@users.noreply.github.com>
2026-09-11 23:56:45 +0530

    feat: dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
@@ -31,8 +33,18 @@ export default function RootLayout({
         geistMono.variable,
         "h-full antialiased typeset-chat",
       )}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
